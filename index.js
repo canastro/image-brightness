17bcb614d33d1cd572f87a3bc9a2682b3c6826b8
canastro <ricardocanastro@gmail.com>
2016-03-06 21:46:48 +0000

    Change brightness bootstrap and comments

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-function copyImageData(ctx, src) {
-    var dst = ctx.createImageData(src.width, src.height);
-    dst.data.set(src.data);
-    return dst;
+/**
+ * @name getCanvas
+ * @param {number} w - width
+ * @param {number} h - height
+ * Create a canvas with the currect size
+ */
+function getCanvas(w, h) {
+    var canvas = document.createElement('canvas');
+    canvas.width = w;
+    canvas.height = h;
+
+    return canvas;
 }
 
+/**
+ * @name getPixels
+ * @param {object} canvas
+ * @param {object} context
+ * @param {object} imageData
+ * Get a deep copy of the image data so we don't change the original imageData
+ */
+function getPixels(canvas, context, imageData) {
+    context.putImageData(imageData, 0, 0);
+    return context.getImageData(0, 0, canvas.width, canvas.height);
+}
+
+/**
+ * @name transform
+ * @param {object} canvas
+ * @param {object} context
+ * @param {object} imageData
+ * Iterate over the array applying the brightness transformation
+ */
 function transform(canvas, context, imageData, adjustment) {
     var data = imageData.data;
 
@@ -27,14 +54,17 @@ module.exports = function imageBrightness(options) {
     var element;
     var data;
     var factor
-    var canvas = document.createElement('canvas');
-    var context = canvas.getContext('2d');
+    var canvas;
+    var context;
 
     if (!options.data || !options.adjustment) {
         throw new Error('image-brightness:: invalid options provided');
     }
 
-    options.data = copyImageData(context, options.data);
+    canvas = getCanvas(options.data.width, options.data.height);
+    context = canvas.getContext('2d');
+
+    options.data = getPixels(canvas, context, options.data);
 
     result = transform(canvas, context, options.data, options.adjustment);
 
